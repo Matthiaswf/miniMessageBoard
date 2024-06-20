ca5a7c9a96e7d907a9763435fbf912330a0a2cd8
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-06-20 16:56:54 +0900

    Project complete including redirect button

--- routes/index.js
+++ routes/index.js
@@ -19,5 +19,17 @@ const messages = [
 router.get("/", (req, res, next) => {
   res.render("index", { messages: messages });
 });
+//get the new page
+router.get("/new", (req, res, next) => {
+  res.render("form");
+});
+
+//post the new message
+router.post("/new", (req, res, next) => {
+  const user = req.body.user;
+  const text = req.body.text;
+  messages.push({ text: text, user: user, added: new Date() });
+  res.redirect("/");
+});
 
 module.exports = router;
