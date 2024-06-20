5dd6532a6604407ae4ab374a6efd637af566e965
Matthiaswf <122594814+Matthiaswf@users.noreply.github.com>
2024-06-20 16:36:09 +0900

    Messsages are displayed on index

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,23 @@
 const express = require("express");
 const router = express.Router();
 
+//messages array
+const messages = [
+  {
+    text: "Hi there!",
+    user: "Amando",
+    added: new Date(),
+  },
+  {
+    text: "Hello World!",
+    user: "Charles",
+    added: new Date(),
+  },
+];
+
 //get the index page
 router.get("/", (req, res, next) => {
-  res.render("index");
+  res.render("index", { messages: messages });
 });
 
 module.exports = router;
